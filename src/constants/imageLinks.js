cdd2d5086855091e0eba3468e7be20c9ef3cee67
Honey171 <103601089+Honey171@users.noreply.github.com>
2023-01-10 18:24:32 +0300

    Create imageLinks.js

---
+++ src/constants/imageLinks.js
@@ -0,0 +1,74 @@
+// Carousel
+export const bigCarouselImageOne =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-Pro-9-All-Colors-FY23HOL:VP1-539x440';
+
+export const bigCarouselImageTwo =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-PC-GamePass-Light-FY23HOL:VP1-539x440';
+
+// Choose section
+export const surface =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/surface-go2-link-list-120x120?wid=40&hei=40';
+
+export const games =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/xbox-blk-logo-link-list-120x120?wid=40&hei=40';
+
+export const windows =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/gldn-Quick-Link-Icon-80x80-Microsoft-365?wid=40&hei=40';
+
+export const accessories =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/keyboard-mouse-link-list-120x120?wid=40&hei=40';
+
+export const business =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/gldn-ICON-LL-briefcase-120x120?wid=40&hei=40';
+
+// Product section
+
+export const productOne =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Xbox-Series-S-FY23HOL?wid=380&hei=213&fit=crop';
+
+export const productTwo =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Surface-Go-3-FY23HOL?wid=380&hei=213&fit=crop';
+
+export const productThree =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Xbox-Series-X-FY23HOL?wid=380&hei=213&fit=crop';
+
+export const productFour =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Surface-Laptop-5-Platinum-Alcantara-FY23HOL?wid=380&hei=213&fit=crop';
+
+// Controller section
+
+export const controller =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Xbox-Controllers-Black-White-FY23HOL:VP5-1596x600';
+
+export const controllerMobile =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Xbox-Controllers-Black-White-FY23HOL:VP1-539x440';
+
+// Business section
+
+export const businessOne =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Surface-Pro-9-LIfestyle-M2?wid=380&hei=213&fit=crop';
+
+export const businessTwo =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/gldn-CP-Microsoft-Teams-Commercial?wid=380&hei=213&fit=crop';
+
+export const businessThree =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/JIC-DPS-CP01?wid=380&hei=213&fit=crop';
+
+export const businessFour =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Content-Card-Brand-Hybrid-Work?wid=380&hei=213&fit=crop';
+
+// Big carousel two
+
+export const bigCarouselTwoImageOne =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-NFL-Seahawks-Tyler-Lockett:VP5-1596x600';
+
+export const bigCarouselTwoImageTwo =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Microsoft-Childrens-Village-Donovan-Mitchell:VP5-1596x600';
+
+// Big carousel two smaller
+
+export const bigCarouselTwoImageOneSmaller =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Surface-NFL-Seahawks-Tyler-Lockett:VP1-539x440';
+
+export const bigCarouselTwoImageTwoSmaller =
+  'https://cdn-dynmedia-1.microsoft.com/is/image/microsoftcorp/Highlight-Microsoft-Childrens-Village-Donovan-Mitchell:VP1-539x440';
